19e0402ea6c1c101b8e71c0cc1a01ef1594d58b1
Jared Velasquez <jaredvel25@gmail.com>
2024-08-29 16:28:12 -0700

    Added plotting of cost and gradient norm

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -9,8 +9,61 @@ import matplotlib.pyplot as plt
 from py_factor_graph.utils.logging_utils import logger
 from py_factor_graph.factor_graph import FactorGraphData
 
+FONT_SIZE = 10
+DPI = 1200
+
 def rbcd_plot(args) -> None:
-    pass
+    dataset = args.dataset
+    output_dir = args.output_dir
+    f_sdp = args.f_sdp
+
+    iter = []
+    robot = []
+    norm_cost = []
+    gradnorm = []
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    logger.info(f"Plotting RBCD output from {dataset} to {output_dir}")
+
+    # Parse RBCD output
+    with open(dataset, "r") as f:
+        for line in f:
+            if not line.startswith("#"): # Skip comments
+                iter.append(int(line.split()[0]))
+                robot.append(int(line.split()[1]))
+                gradnorm.append(float(line.split()[3]))
+
+                # Normalize cost
+                cost = float(line.split()[2])
+                norm_cost.append((cost - f_sdp) / f_sdp)
+    
+    # Plot iterations vs. normalized cost
+    plt.figure()
+    plt.rcParams['figure.facecolor'] = 'white'
+    # plt.gca().set_aspect('equal')
+    plt.plot(iter, norm_cost)
+    plt.xlabel("Iteration", fontsize=FONT_SIZE)
+    plt.ylabel("Normalized Cost", fontsize=FONT_SIZE)
+    plt.title("Iterations vs. Normalized Cost", fontsize=FONT_SIZE)
+    plt.grid(True, color='gray')
+    plt.tight_layout()
+    plt.savefig(os.path.join(output_dir, "iter_vs_cost.png"), dpi=DPI)
+
+    # Plot iterations vs. gradnorm
+    plt.figure()
+    plt.rcParams['figure.facecolor'] = 'white'
+    # plt.gca().set_aspect('equal')
+    plt.plot(iter, gradnorm)
+    plt.xlabel("Iteration", fontsize=FONT_SIZE)
+    plt.ylabel("Gradnorm", fontsize=FONT_SIZE)
+    plt.title("Iterations vs. Gradnorm", fontsize=FONT_SIZE)
+    plt.grid(True, color='gray')
+    plt.tight_layout()
+    plt.savefig(os.path.join(output_dir, "iter_vs_gradnorm.png"), dpi=DPI)
+    
+
 
 def main(args):
     parser = argparse.ArgumentParser(
@@ -29,6 +82,13 @@ def main(args):
         type=str,
         required=True,
         help="directory where results are saved",
+    )
+    parser.add_argument(
+        "-f",
+        "--f_sdp",
+        type=float,
+        default=1.0,
+        help="scaling factor for the SDP cost",
     )
 
     args = parser.parse_args()
